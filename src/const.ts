707734cf5e6e920be68d1233667436693d5f0413
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-02-02 14:02:27 +0300

    feat: Удалит из компонента главной страницы все статичные карточки предложений. Вместо них подключит компонент карточки предложения.

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,12 @@
 export const Setting = {
   PlaceCardsCount: 5
 };
+
+export const CITIES = {
+  PARIS: 'Paris',
+  COLOGNE: 'Cologne',
+  BRUSSELS: 'Brussels',
+  AMSTERDAM: 'Amsterdam',
+  HAMBURG: 'Hamburg',
+  DUSSELDORF: 'Dusseldorf'
+};
