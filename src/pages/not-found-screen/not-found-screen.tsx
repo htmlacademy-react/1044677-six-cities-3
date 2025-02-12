e978feba22f08a5d6dbf4a7591a4a83e06efafc8
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-02-12 19:17:50 +0300

    feat: Создаёт компонент для страницы Not-Found

---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,9 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <section className="not-found__screen">
+      <h1>404. Page not found</h1>
+      <a href="/">Вернуться на главную</a>
+    </section>
+  );
+}
+export default NotFoundScreen;
