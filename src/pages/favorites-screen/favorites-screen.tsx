491561f3740f45d27da82821d0ab3e7580d3688b
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-03-20 23:48:05 +0300

    feat: Добавит  функционал открытия карточки оффера со старницы Favorites

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Offer } from '../../types/offer';
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
@@ -16,14 +17,14 @@ function FavoriteCard({offer}: { offer: Offer }): JSX.Element {
         </div>
       )}
       <div className="favorites__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <Link to={`/offer/${offer.id}`}>
           <img className="place-card__image"
             src={offer.img}
             width="150"
             height="110"
             alt="Place image"
           />
-        </a>
+        </Link>
       </div>
       <div className="favorites__card-info place-card__info">
         <div className="place-card__price-wrapper">
@@ -45,7 +46,7 @@ function FavoriteCard({offer}: { offer: Offer }): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{offer.placeCardName}</a>
+          <Link to={`/offer/${offer.id}`}>{offer.placeCardName}</Link>
         </h2>
         <p className="place-card__type">{offer.placeCardType}</p>
       </div>
