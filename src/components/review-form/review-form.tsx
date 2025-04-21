8b6a30ae2b233a5aa307512a49cef8e7d061c279
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-04-21 23:47:05 +0300

    feat: Дополнит условия валидации формы отзыва

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -59,11 +59,12 @@ function ReviewForm(): JSX.Element {
       />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay.
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
         <button
           className="reviews__submit form__submit button"
           type="submit"
+          disabled={formData.rating === 0 || formData.review.length < 50 || formData.review.length > 300}
         >
           Submit
         </button>
