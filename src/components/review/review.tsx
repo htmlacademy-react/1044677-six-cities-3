11fbb335a799f38528924a3323b15e2c9118d8bd
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-05-03 22:53:07 +0300

    feat: Создаст компонент отзыва

---
+++ src/components/review/review.tsx
@@ -0,0 +1,37 @@
+import { Review as ReviewType } from '../../types/review';
+
+type ReviewProps = {
+  review: ReviewType;
+}
+
+function Review({review}: ReviewProps): JSX.Element {
+  const {user, rating, comment, date} = review;
+  const formatDate = new Date(date).toLocaleDateString('en-US', {month: 'long', year: 'numeric' });
+
+  return (
+    <li className="reviews__item">
+      <div className="reviews__user user">
+        <div className="reviews__avatar-wrapper user__avatar-wrapper">
+          <img className="reviews__avatar user__avatar" src={user.avatar} width="54" height="54" alt="Reviews avatar" />
+        </div>
+        <span className="reviews__user-name">
+          {user.name}
+        </span>
+      </div>
+      <div className="reviews__info">
+        <div className="reviews__rating rating">
+          <div className="reviews__stars rating__stars">
+            <span style={{width: `${rating}%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <p className="reviews__text">
+          {comment}
+        </p>
+        <time className="reviews__time" dateTime={date}>{formatDate}</time>
+      </div>
+    </li>
+  );
+}
+
+export default Review;
