025d3439d5374d924ef25c0baac2451844772652
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-06-07 20:49:14 +0300

    feat: Создаст файл описания действий по смене города и заполнению офферов

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,8 @@
-import { changeCity } from './action';
 import { DEFAULT_CITY } from '../const';
 import { offers } from '../mocks/offers';
 import { createReducer } from '@reduxjs/toolkit';
 import { OfferState } from '../types/offer-state';
+import { changeCity, fillOffers } from './action';
 
 const initialState: OfferState = {
   city: DEFAULT_CITY,
@@ -13,5 +13,8 @@ export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeCity, (state, action) => {
       state.city = action.payload;
+    })
+    .addCase(fillOffers, (state, action) => {
+      state.offers = action.payload;
     });
 });
