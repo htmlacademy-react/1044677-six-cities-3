b96823d395bd64a1c91d118f8651504691a7b1d9
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-06-24 19:34:19 +0300

    feat: Добавит новое действие загрузки офферов

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,14 @@
 import { SortType } from '../const';
 import { City } from '../types/city';
+import { AxiosInstance } from 'axios';
 import { Offers } from '../types/offer';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const changeCity = createAction<City>('city/change');
-export const fillOffers = createAction<Offers>('offers/fill');
 export const changeSortType = createAction<SortType>('sort/change');
+
+export const fetchOffers = createAsyncThunk<Offers, void, {extra: AxiosInstance}>
+('offers/fetchOffers', async (_arg, { extra: api }) => {
+  const response = await api.get<Offers>('/offers');
+  return response.data;
+});
