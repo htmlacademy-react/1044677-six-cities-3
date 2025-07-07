3856106a0c9cc85fb9ca58a69fe183ebb211a116
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-07-07 20:29:35 +0300

    feat: Дополнит типизацию офферов для взаимодействия с сервером

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -19,7 +19,30 @@ export type Offer = {
   isFavorite: boolean;
   isPremium: boolean;
   rating: number;
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: {
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+  };
+  images: string[];
+  maxAdults: number;
   previewImage: string;
 };
 
 export type Offers = Offer[];
+
+export type PlaceCard = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  previewImage: string;
+  city: City;
+  location: Location;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+};
